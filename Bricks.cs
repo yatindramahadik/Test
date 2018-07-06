ede0316050d0ac0948f3ecb2778d4c547fa3d04b
Yatindra Mahadik <mahadik.yatindra@gmail.com>
2018-07-06 12:55:44 +0530

    initial project version

---
+++ Bricks.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bricks : MonoBehaviour {
+    //private int maxHits;
+    public Sprite[] hitSprites;
+    public static int breakableCounts = 0;
+    public AudioClip crack;
+    public GameObject smoke;
+
+    private int timesHit;
+    private LevelManager levelManager;
+    private bool isBreakable;
+
+	// Use this for initialization
+	void Start () {
+        isBreakable = (this.tag == "Breakable");
+        if (isBreakable){
+            breakableCounts++;
+            //print(breakableCounts);
+        }
+        timesHit = 0;
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        AudioSource.PlayClipAtPoint(crack, transform.position);
+        if (isBreakable){
+            HandleHits();
+        }
+    }
+    void HandleHits(){
+        timesHit++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHit >= maxHits)
+        {
+            breakableCounts--;
+            levelManager.BrickDestroyed();
+            GameObject smokePuff = Instantiate(smoke, transform.position, Quaternion.identity) as GameObject;
+#pragma warning disable CS0618 // Type or member is obsolete
+            smokePuff.GetComponent<ParticleSystem>().startColor = gameObject.GetComponent<SpriteRenderer>().color;
+#pragma warning restore CS0618 // Type or member is obsolete
+            Destroy(gameObject);
+        }
+        else
+        {
+            LoadSprites();
+        }
+
+    }
+
+
+    void LoadSprites(){
+        int spriteIndex = timesHit - 1;
+        if (hitSprites[spriteIndex] != null)
+        {
+
+            this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+            Debug.LogError("gayab hai ");
+    }
+    //TODO remove this
+    void SimulateWin(){
+        levelManager.LoadNextLevel();
+    }
+}
---
+++ Paddle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    // Use this for initialization
+    public bool autoPlay = false;
+    private Ball ball;
+	
+    private void Start()
+	{
+        ball = GameObject.FindObjectOfType<Ball>();
+        print(ball);
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (autoPlay == false)
+        {
+            MoveWithMouse();
+        } else {
+            AutoPlay();
+        }
+	}
+
+    void AutoPlay(){
+        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
+        Vector3  ballPos = ball.transform.position;
+        //print(mousePosInBlocks);
+        paddlePos.x = Mathf.Clamp(ballPos.x, 0.5f, 15.5f);
+        this.transform.position = paddlePos;
+
+    }
+    void MoveWithMouse (){
+        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
+        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+        //print(mousePosInBlocks);
+        paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+        this.transform.position = paddlePos;
+
+    }
+}
